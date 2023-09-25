b5b2953bd1f4337cef7a38ccde7358e1663a243a
cola777jz <cola777jz@gmail.com>
2023-09-25 23:38:21 +0800

    dog

---
+++ java_homework/src/main/java/ygnn/cola/study/day14/Dog.java
@@ -0,0 +1,14 @@
+package ygnn.cola.study.day14;
+
+/**
+ * @author Cola0817
+ * @name Dog
+ * @date 2023/9/25 23:05
+ * @since 1.0.0
+ */
+public class Dog extends Animal{
+    @Override
+    public void sayHello() {
+        System.out.println("dog sayHello ");
+    }
+}
