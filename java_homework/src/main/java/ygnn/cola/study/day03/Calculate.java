21b72566109190ab3673dba90d6a6bda609febba
cola777jz <cola777jz@gmail.com>
2023-09-25 23:37:31 +0800

    Calculate

---
+++ java_homework/src/main/java/ygnn/cola/study/day03/Calculate.java
@@ -0,0 +1,18 @@
+package ygnn.cola.study.day03;
+
+/**
+ * @author Cola0817
+ * @name Calculate
+ * @date 2023/9/25 22:24
+ * @since 1.0.0
+ */
+public class Calculate {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 2;
+        System.out.println("a + b = " + (a + b));
+        System.out.println("a - b = " + (a - b));
+        System.out.println("a * b = " + (a * b));
+        System.out.println("a / b = " + (a / b));
+    }
+}
