f129ef0758c54ec3ba7a3ba0256d702ee502b794
cola777jz <cola777jz@gmail.com>
2023-09-25 23:39:19 +0800

    sin

---
+++ java_homework/src/main/java/ygnn/cola/study/day20/SinDomain.java
@@ -0,0 +1,19 @@
+package ygnn.cola.study.day20;
+
+/**
+ * @author Cola0817
+ * @name SinDomain
+ * @date 2023/9/25 23:34
+ * @since 1.0.0
+ */
+public class SinDomain {
+    public static void main(String[] args) {
+        double sum = 0.0;
+        long start = System.currentTimeMillis();
+        for (int i = 0; i < 1000_0000; i++) {
+            sum += Math.sin(i);
+        }
+        long end = System.currentTimeMillis();
+        System.out.println((end - start) + " ms");
+    }
+}
