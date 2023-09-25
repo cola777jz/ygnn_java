3e8b02b9f2bd09f08d4b7b0d0423b8ee1b6c1c52
cola777jz <cola777jz@gmail.com>
2023-09-25 23:39:53 +0800

    variable

---
+++ java_homework/src/main/java/ygnn/cola/study/day02/Variable.java
@@ -0,0 +1,18 @@
+package ygnn.cola.study.day02;
+
+/**
+ * @author Cola0817
+ * @name Variable
+ * @date 2023/9/25 22:21
+ * @since 1.0.0
+ */
+public class Variable {
+    public static void main(String[] args) {
+        String name = "BaoQi Cao";
+        int age = 21;
+        String school = "JinZhong Collage";
+
+        System.out.println("name = " + name + " age = " + age + " school = " + school);
+
+    }
+}
