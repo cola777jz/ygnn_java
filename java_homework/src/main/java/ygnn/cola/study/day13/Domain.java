ddab41d5b2c8d7da6d9160e991add06945cf05b7
cola777jz <cola777jz@gmail.com>
2023-09-25 23:38:26 +0800

    domain

---
+++ java_homework/src/main/java/ygnn/cola/study/day13/Domain.java
@@ -0,0 +1,16 @@
+package ygnn.cola.study.day13;
+
+/**
+ * @author Cola0817
+ * @name Domain
+ * @date 2023/9/25 22:59
+ * @since 1.0.0
+ */
+public class Domain {
+    public static void main(String[] args) {
+        Cat cat = new Cat(7, "Tom");
+        cat.eat();
+
+        StaticDemo.sayPI();
+    }
+}
