79e80554a6b2feb7aa7d257dd1c97b4145ba64d9
cola777jz <cola777jz@gmail.com>
2023-09-27 23:38:26 +0800

    scanner

--- java_book/src/main/java/ygnn/cola/study/chapter12/example/Example_04.java
+++ java_book/src/main/java/ygnn/cola/study/chapter12/example/Example_04.java
@@ -1,6 +1,10 @@
 package ygnn.cola.study.chapter12.example;
 
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 /**
  * @author Cola0817
  * @name Example_04
@@ -8,5 +12,22 @@ package ygnn.cola.study.chapter12.example;
  * @since 1.0.0
  */
 public class Example_04 {
+    /**
+     * 统计 record.txt 中的数据
+     * @param args
+     */
+    public static void main(String[] args) throws FileNotFoundException {
+        Scanner reader = new Scanner(new File("E:\\work_space\\IDEA\\ygnn_java\\java_book\\src\\main\\java\\ygnn\\cola\\study\\chapter12\\example\\record.txt"));
+        double price, total = 0.0;
 
+        reader.useDelimiter("[^0-9.]+");
+
+        while (reader.hasNext()){
+            price = reader.nextDouble();
+            total += price;
+        }
+        System.out.println("total = " + total);
+
+
+    }
 }
