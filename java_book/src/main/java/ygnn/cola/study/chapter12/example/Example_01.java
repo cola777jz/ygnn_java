e315574150f2f3fb213510014b8ce68b9009b704
cola777jz <cola777jz@gmail.com>
2023-09-27 23:18:17 +0800

    file field read

---
+++ java_book/src/main/java/ygnn/cola/study/chapter12/example/Example_01.java
@@ -0,0 +1,45 @@
+package ygnn.cola.study.chapter12.example;
+
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+/**
+ * @author Cola0817
+ * @name Example_01
+ * @date 2023/9/27 21:57
+ * @since 1.0.0
+ */
+public class Example_01 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please input filename: ");
+        String fileName = scanner.nextLine();
+        File file = new File(fileName);
+
+        System.out.println("file.getName() = " + file.getName());
+        System.out.println("file.length() = " + file.length());
+        System.out.println("file.getAbsoluteFile() = " + file.getAbsoluteFile());
+
+        if (file.isHidden()){
+            System.out.println("file is hidden");
+        }else {
+            System.out.println("file is not hidden");
+        }
+
+        if (!file.exists()){
+            System.out.println("file is not exist|");
+            try {
+                if (file.createNewFile()) {
+                    System.out.println("create successful");
+                }else {
+                    System.out.println("create fail");
+                }
+
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
